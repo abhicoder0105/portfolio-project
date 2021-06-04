90bd9f40a6e1c7dde65fe77233f34ef15ca29d51
abhicoder0105 <sharmaabhishek9770@gmail.com>
2021-06-04 16:13:00 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+const App = () => {
+  return (
+    <div>
+      <h1>Hello there</h1>
+    </div>
+  )
+}
+
+export default App
