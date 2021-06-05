285194ffadd1742c60d63bb142fc356679b48687
abhicoder0105 <sharmaabhishek9770@gmail.com>
2021-06-05 19:14:43 +0530

    Navbar Completed

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
+import Navbar from './components/Navbar';
+import './App.css'
+
 
 const App = () => {
   return (
     <div>
-      <h1>Hello there</h1>
+     <Navbar/>
     </div>
   )
 }
